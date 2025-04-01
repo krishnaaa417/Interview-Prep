7e4f36aaddedd34fa0cf53b981c80842207a728f
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-01 19:59:24 +0530

    some more basic codes

---
+++ Interview-Prep/MaxNdMin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class MaxNdMin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Max value is A.." + a);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("Max value is B.." + b);
+            }
+            else
+            {
+                Console.WriteLine("values are same");
+            }
+        }
+    }
+}
---
+++ Interview-Prep/SubtractTwoNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class SubtractTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sub(10, 20));
+            Console.WriteLine(Mul(10, 20));
+            Console.WriteLine(Div(10, 20));
+            Console.WriteLine(Mod(10, 20));
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static  int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static  int Mod(int a, int b)
+        {
+            return (a % b);
+        }
+    }
+}
