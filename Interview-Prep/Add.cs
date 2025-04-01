d0f3bbab8e36d6fdee14856dda23724b9c167a65
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-01 18:42:40 +0530

    Add project files.

---
+++ Interview-Prep/Add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class Add
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+
+            Console.WriteLine("sum of your two numbers is.."+c);
+        }
+    }
+}
