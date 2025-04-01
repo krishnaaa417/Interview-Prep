74fa81601e24cc67ae84c517ab23a286053f7632
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-01 23:18:33 +0530

    even or odd

---
+++ Interview-Prep/EvenOdd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("please enter a number..");
+            int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please enter second number..");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Even-Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd-Number..");
+            }
+
+
+        }
+    }
+}
