d057c789d296d4b7493d549f36b93edf5c7b015d
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-02 12:02:37 +0530

    Secondlargest Element in A given Array when it is SORTED ARRAY.

---
+++ Interview-Prep/SecondLargestNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class SecondLargestNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 33,33333, 222, 1,  33144 };
+            int largest = int.MinValue;
+            int secondLargest = int.MinValue;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > largest)
+                {
+                    secondLargest = largest;
+                    largest = a[i];
+                }
+            }
+           
+           
+        }
+    }
+}
