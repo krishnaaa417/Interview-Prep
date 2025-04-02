d227276fd47eb23757b47edd66f661511ec682bf
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-02 11:16:21 +0530

    Soome more problemss

---
+++ Interview-Prep/LargestNumberInArry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class LargestNumberInArry
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 7, 1, 9, 6 };
+            int temp = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (temp < a[i])
+                {
+                    temp = a[i];
+                }
+            }
+            Console.WriteLine("Largest element in an AArray.."+ temp);
+        }
+    }
+}
---
+++ Interview-Prep/SmallestNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class SmallestNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 12, 23, 44, 21, 22 };
+            int max = int.MaxValue;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < max)
+                {
+                    max = a[i];
+                }
+            }
+
+            Console.WriteLine("Smallest Value in the array."+max);
+        }
+    }
+}
