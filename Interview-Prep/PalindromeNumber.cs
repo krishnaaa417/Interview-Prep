5737f0605b5e75b6d8de0eb815e4e374c6e2b8dc
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-09 10:04:29 +0530

    PalindromeNumber

---
+++ Interview-Prep/PalindromeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class PalindromeNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = 121;
+            int reversenumber = 0;
+            int palindrome = number;
+
+            while (number > 0)
+            {
+                int num = number % 10;
+                reversenumber = reversenumber * 10 + num;
+                number = number / 10;
+            }
+
+            if (reversenumber == palindrome)
+            {
+                Console.WriteLine("Your number is Palindrome..");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
