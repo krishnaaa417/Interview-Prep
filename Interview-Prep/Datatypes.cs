d9eb1e76f4f18213cb1d6420afb037c2d03a5ea1
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-12 14:28:26 +0530

    DataTypes in C#

---
+++ Interview-Prep/Datatypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    internal class Datatypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a integer");
+            int i = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(i);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine("please enter your mobile number");
+            long phonenumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("your contact number is.." + phonenumber);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            //Console.WriteLine("------------");
+            //Console.WriteLine("please enter a Float number");
+            //float f = Convert.ToSingle(Console.ReadLine());
+            //Console.WriteLine(f);
+            //Console.WriteLine(float.MinValue);
+            //Console.WriteLine(float.MaxValue);
+
+            Console.WriteLine("Double.");
+            Console.WriteLine("please enter your salary");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine("000----");
+        }
+    }
+}
