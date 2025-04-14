e7ab7f1d05fbd3a7a1c57487aad13dafa6769750
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-14 19:18:43 +0530

    Number Pattern

--- Interview-Prep/Patterns/NumberPattern1.cs
+++ Interview-Prep/Patterns/NumberPattern1.cs
@@ -10,7 +10,28 @@ namespace Interview_Prep.Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Number-Pattern");
+            NumberPattern1.NumberPattern();
+        }
+
+        //
+        //1
+        //2 2
+        //3 3 3
+        //4 4 4 4
+        //5 5 5 5 5
+        public static void NumberPattern()
+        {
+            Console.WriteLine("please enter the row number");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + "  ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Interview-Prep/SRE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep
+{
+    /*
+    A class should have only one reason to change, meaning it should have only one job or responsibilty.
+    */
+    internal class SRE
+    {
+        public void ReportFile()
+        {
+
+        }
+
+        public void GenerateFile()
+        {
+
+        }
+    }
+}
