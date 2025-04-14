e1e72743010f15e3cbcdde33dbd5a7c1006acfff
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-14 22:27:17 +0530

    Some more patterns

--- Interview-Prep/Patterns/NumberPattern1.cs
+++ Interview-Prep/Patterns/NumberPattern1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,40 @@ namespace Interview_Prep.Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Number-Pattern");
+            //NewMethod();
+            Reversing();
+        }
+
+        private static void NewMethod()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Expected-Output
+        //7 7 7 7 7 7
+        //6 6 6 6 6
+        //5 5 5 5 
+        //4 4 4
+        //3 3
+        //2
+
+        private static void Reversing()
+        {
+          for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(10-i + "  ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Interview-Prep/Patterns/NumberPattern4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep.Patterns
+{
+    internal class NumberPattern4
+    {
+        static void Main(string[] args)
+        {
+            // NumberPattern4.NumberPattern();
+            NumberPattern4.NumberPattern2();
+        }
+
+        private static void NumberPattern()
+        {
+            int rows = 9;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void NumberPattern2()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i-j+1+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
