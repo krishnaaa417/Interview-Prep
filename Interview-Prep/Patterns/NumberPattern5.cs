3842c51e2f5f2391a24ee70569493180c5848683
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-15 09:49:28 +0530

    patterns-2nd day

---
+++ Interview-Prep/Patterns/NumberPattern5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep.Patterns
+{
+    internal class NumberPattern5
+    {
+        static void Main(string[] args)
+        {
+            int rows = 7;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(rows-j-i + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Interview-Prep/Patterns/StarPattern1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prep.Patterns
+{
+    internal class StarPattern1
+    {
+        static void Main(string[] args)
+        {
+            //StarPattern1.Star();
+            StarPattern1.Test();
+        }
+
+        public static void Star()
+        {
+            int rows = 6;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write("-");
+                }
+
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+}
